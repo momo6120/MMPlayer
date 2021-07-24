423933b607ee51dc431f0546b09f9affdf6e25aa
momo6120 <xsungg@gmail.com>
2021-07-24 23:13:30 +0800

    C++线程类

---
+++ MMPlayer/MMPlayer.cpp
@@ -0,0 +1,23 @@
+﻿// MMPlayer.cpp: 定义应用程序的入口点。
+//
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "MMThread/MMThread.h"
+
+class MyMMThread :public MMThread
+{
+public:
+	virtual void run()
+	{
+		std::cout << "MyMMThread" << std::endl;
+	}
+};
+int main()
+{
+	MyMMThread t;
+	t.start();
+
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	return 0;
+}
